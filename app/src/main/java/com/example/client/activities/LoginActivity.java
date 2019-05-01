6c0dd0da6fa78a8ddc50d1aef88e965edd82bbe8
Gabi <gabrielserbanescu0@gmail.com>
2019-05-01 14:17:40 +0300

    added activities for login, register and main activity(dashboard)

--- app/src/main/java/com/example/client/activities/LoginActivity.java
+++ app/src/main/java/com/example/client/activities/LoginActivity.java
@@ -1,15 +1,43 @@
 package com.example.client.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.client.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnLogin,btnRegister;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnRegister=findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/client/activities/RegisterActivity.java
+++ app/src/main/java/com/example/client/activities/RegisterActivity.java
@@ -1,15 +1,32 @@
 package com.example.client.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.client.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister=findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
